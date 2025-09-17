4add64ff575b057ccd1bc4a9816b2afdf8cd9575
andersmx3199 <and20004@byui.edu>
2025-09-16 21:16:40 -0600

    added comments for prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,36 +7,35 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Character variable for the letter grade from if-else if-else statements
         char letter;
 
+        // Introduction message to provide grade percentage. Converted from string to int.
         Console.Write("Hello, please provide your grade percentage: ");
         string number = Console.ReadLine();
         int percent = int.Parse(number);
 
+        // If-else if-else statements to present letter grade. Comments inside statements are from core part 1.
         if (percent >= 90)
         {
             //Console.WriteLine("Your letter grade is an A.");
             letter = 'A';
         }
-
         else if (percent >= 80 && percent < 90)
         {
             //Console.WriteLine("Your letter grade is a B.");
             letter = 'B';
         }
-
         else if (percent >= 70 && percent < 80)
         {
             //Console.WriteLine("Your letter grade is a C.");
             letter = 'C';
         }
-
         else if (percent >= 60 && percent < 70)
         {
             //Console.WriteLine("YOur letter grade is a D.");
             letter = 'D';
         }
-
         else
         {
             //Console.WriteLine("Your letter grade is an F.");
@@ -44,10 +43,10 @@ class Program
         }
 
 
-        // Core 3, present letter grade
+        // Core 3, present letter grade with a variable rather than in each if statement.
         Console.WriteLine($"Your letter grade is a[n] {letter}.");
 
-        // Pass the class or not
+        // Pass the class or not. If percent is over 70% then they pass, if not give encouragment to try again.
         if (percent >= 70)
         {
             Console.WriteLine("Congrats! You passes the class!");
