a16d0dd909c1d6c9a97760d7783fff22047c7102
andersmx3199 <and20004@byui.edu>
2025-09-24 08:36:37 -0600

    Completed Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -25,8 +25,19 @@ class Program
         // Number of numbers in the list
         Console.WriteLine($"Numbers in the list: {numbers.Count}");
 
+        // Core 1
         // Add the items of the list together to make the sum of the list.
         int Sum = numbers.Sum();
         Console.WriteLine($"The sum is: {Sum}");
+
+        // Core 2
+        // Find the average of the list (use the Sum variable from before)
+        double average = (double)Sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Core 3
+        // Find the maximum number in the list.
+        int maxNum = numbers.Max();
+        Console.WriteLine($"The largest number is {maxNum}");
     }
 }
