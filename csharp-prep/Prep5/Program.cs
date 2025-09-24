c0c0758609a5f5766d2d91e0e26fceefa563355f
andersmx3199 <and20004@byui.edu>
2025-09-24 08:42:48 -0600

    Started Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Program welcome
+        Console.WriteLine("Welcome to the program! ");
+
+        // Enter name to put into name function
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        // Enter favorite number to put into number function
+        Console.Write("Please enter your favorite number: ");
+        string number = Console.ReadLine();
+        int userNum = int.Parse(number);
+
+        // Enter birth year for function
+        Console.Write("Please enter the year you were born: ");
+        string year = Console.ReadLine();
+        int userYear = int.Parse(year);
     }
 }
