d6d99a22666325bfcd949887da8cbed34df2e3ff
andersmx3199 <and20004@byui.edu>
2025-10-11 17:03:17 -0600

    Almost completed Developer 2, needs additional work. I plan to complete this at a future time (spent several hours on it today). I will submit what I have at this time

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -4,15 +4,46 @@ class Menu
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Journal Program!");
-        Console.WriteLine("Please select one of the following choices: ");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        // Get the number
-        string choice = Console.ReadLine();
-        int menuChoice = int.Parse(choice);
+        int menuChoice = 0;
+
+        while (menuChoice != 5)
+        {
+            Console.WriteLine("\nWelcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            // Get the number
+            string choice = Console.ReadLine();
+            menuChoice = int.Parse(choice);
+
+            if (menuChoice == 1)
+            {
+                Console.WriteLine("Here is a prompt for you to respond to:");
+                PromptGen newPrompt = new PromptGen();
+                newPrompt.PickandDisplayPrompt();
+            }
+            else if (menuChoice == 2)
+            {
+                Console.WriteLine("2. Display");
+            }
+            else if (menuChoice == 3)
+            {
+                Console.WriteLine("3. Load");
+            }
+            else if (menuChoice == 4)
+            {
+                Console.WriteLine("4. Save");
+            }
+            else if (menuChoice != 5)
+            {
+                Console.WriteLine("Invalid choice, try again");
+            }
+        }
+
+        Console.WriteLine("Thank you for using the journal program!");
     }
 }
--- prove/Develop02/PromptGen.cs
+++ prove/Develop02/PromptGen.cs
@@ -2,20 +2,43 @@ using System.Security.Cryptography.X509Certificates;
 
 class PromptGen
 {
-    static void Main(string[] args)
+    public string _promptOne = "Who was the most interesting person I interacted with today?";
+    public string _promptTwo = "What was the best part of my day?";
+    public string _promptThree = "How did I see the hand of the Lord in my life today?";
+    public string _promptFour = "What was the strongest emotion I felt today?";
+    public string _promptFive = "If I had one thing I could do over today, what would it be?";
+
+    public Entry PickandDisplayPrompt()
     {
-        public string _promptOne = "";
-        public string _promptTwo = "";
-        public string _promptThree = "";
-        public string _promptFour = "";
-        public string _promptFive = "";
-
-        public void PickPrompt(){
-
-        } 
-
-        public void DisplayPrompt(){
+        Random rand = new Random();
+        int number = rand.Next(1, 6); // 1 is inclusive, 6 is exclusive
+        
+        if (number == 1){
+            Console.WriteLine(_promptOne);
+            string response = Console.ReadLine();
+            return new Entry(_promptOne, response);
+        }
+        else if (number == 2){
+            Console.WriteLine(_promptTwo);
+            string response = Console.ReadLine();
+            return new Entry(_promptTwo, response);
+        }
+        else if (number == 3){
+            Console.WriteLine(_promptThree);
+            string response = Console.ReadLine();
+            return new Entry(_promptThree, response);
+        }
+        else if (number == 4){
+            Console.WriteLine(_promptFour);
+            string response = Console.ReadLine();
+            return new Entry(_promptFour, response);
+        }
+        else{
+            Console.WriteLine(_promptFive);
+            string response = Console.ReadLine();
+            return new Entry(_promptFive, response);
             
         }
-}
+    }
+
 }
