e257c3cc4a0e4cd95964443c89378e19f350597a
andersmx3199 <and20004@byui.edu>
2025-11-19 08:41:27 -0700

    Fixed white space

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -7,6 +7,7 @@ class Program
 
             bool running = true;
             while (running){
+                Console.WriteLine("");
                 Console.WriteLine("Menu Options:");
                 Console.WriteLine("1. Create New Goal");
                 Console.WriteLine("2. List Goals");
@@ -15,38 +16,44 @@ class Program
                 Console.WriteLine("5. Record Event");
                 Console.WriteLine("6. Quit");
 
-                Console.Write("Select an option (1-5): ");
+                Console.Write("Select an option (1-6): ");
                 string input = Console.ReadLine();
                 Console.WriteLine();
 
                 switch (input)
                 {
                     case "1":
+                        Console.WriteLine("");
                         //myJournal.AddEntry(); -- call the created goal to enter a new goal
                         break;
 
                     case "2":
+                        Console.WriteLine("");
                         //myJournal.ShowAllEntries(); -- care the created goal to share the goals method
                         break;
 
                     case "3":
+                        Console.WriteLine("");
                         //myJournal.SaveToFile(); -- call created goal and save to a file
                         break;
 
                     case "4":
+                        Console.WriteLine("");
                         //myJournal.LoadFromFile(); 
                         break;
                     
                     case "5":
+                        Console.WriteLine("");
                         break;
 
                     case "6":
                         running = false;
                         Console.WriteLine("Exiting program. Goodbye!");
+                        Console.WriteLine("");
                         break;
 
                     default:
-                        Console.WriteLine("Invalid selection. Please choose 1-5.");
+                        Console.WriteLine("Invalid selection. Please choose 1-6.");
                         break;
                 }
             }
