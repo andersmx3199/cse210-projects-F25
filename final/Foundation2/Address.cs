0073ec5908c8bfbe5e13bbadcea450d58a46db6a
andersmx3199 <and20004@byui.edu>
2025-12-01 11:02:59 -0700

    completed customer and address classes for Foundation2

---
+++ final/Foundation2/Address.cs
@@ -0,0 +1,27 @@
+class Address
+{
+    // Attributes for Address Class
+    private string _houseStreet = "";
+    private string _city = "";
+    private string _state = "";
+    private string _country = "";
+
+    // Behaviors for Address Class
+    public Address(string houseStreet, string city, string state, string country)
+    {
+        _houseStreet = houseStreet;
+        _city = city;
+        _state = state;
+        _country = country;
+    }
+
+    public bool InUSA()
+    {
+        return _country.ToLower() == "usa";
+    }
+
+    public string GetAddress()
+    {
+        return ($"{_houseStreet}\n{_city}, {_state}\n{_country}");
+    }
+}
