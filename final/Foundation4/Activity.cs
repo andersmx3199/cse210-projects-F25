7347d0337492cee62879f39efdb589a2a95306cf
andersmx3199 <and20004@byui.edu>
2025-12-03 18:30:30 -0700

    Corrected digit display for distance, speed, and pace with Math.Round and 0:00 when GetDistance(), GetSpeed(), and GetPace() are called

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -20,6 +20,6 @@ abstract class Activity
 
     public string GetSummary()
     {
-        return ($"{_date} {_activity} ({_duration}mins) -  Distance {GetDistance()} miles, Speed {GetSpeed()} mph, Pace {GetPace()} min per mile");
+        return ($"{_date} {_activity} ({_duration}mins) -  Distance {GetDistance():0.00} miles, Speed {GetSpeed():0.00} mph, Pace {GetPace():0.00} min per mile");
     }
 }
